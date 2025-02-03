892f2bc20ad5c6c5281cf7ee39411803e0b08ac4
Nikola Stankovic <nikola.stankovic@viascom.ch>
2025-02-03 21:20:30 +0100

    review: add copyright

--- src/main/kotlin/io/viascom/nanoid/NanoId.kt
+++ src/main/kotlin/io/viascom/nanoid/NanoId.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2023-2025 Viascom Ltd liab. Co
+ * Copyright 2025 Viascom Ltd liab. Co
  *
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
