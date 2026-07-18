a4ce9b5ff8b8fbb6165f8ec822d31f4401d633b4
itsmefox <patrick.boesch@viascom.ch>
2026-07-18 11:25:19 +0200

    build: replace JVM build with Kotlin Multiplatform build script

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform") version "2.4.10"
+}
+
+val major = 2
+val minor = 0
+val patch = 0
+
+val isCiServer = System.getenv("GITHUB_ACTIONS") != null || System.getProperty("GITHUB_ACTIONS") != null
+
+group = "io.viascom.nanoid"
+version = "$major.$minor.$patch${if (isCiServer) "" else "-SNAPSHOT"}"
+logger.lifecycle("Version of this build: $version")
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    jvm()
+
+    js {
+        browser {
+            testTask {
+                useKarma { useChromeHeadless() }
+            }
+        }
+        nodejs()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            testTask {
+                useKarma { useChromeHeadless() }
+            }
+        }
+        nodejs()
+    }
+
+    macosX64()
+    macosArm64()
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+    tvosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    watchosArm32()
+    watchosArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    linuxX64()
+    linuxArm64()
+    mingwX64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation("dev.whyoleg.cryptography:cryptography-random:0.6.0")
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+tasks.withType<Jar>().configureEach {
+    manifest {
+        attributes["Implementation-Version"] = project.version
+    }
+}
